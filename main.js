a3ec8b5c9e48f523b20b4442ce2a02859307d753
Medali-Bouajila <medali.bouajila09@gmail.com>
2024-07-24 19:53:16 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+ let colorBox = document.getElementById("color-box");
+  let changeColorBtn = document.getElementById("change-color-btn");
+
+  function getRandomColor() {
+    let letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  changeColorBtn.addEventListener("click", function () {
+    colorBox.style.backgroundColor = getRandomColor();
+  });
+});
